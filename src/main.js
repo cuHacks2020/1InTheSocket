2affb5f28e49b760e30b84209da12bb995608ddd
Devon Mack <devon.mack@shopify.com>
2020-01-11 12:54:38 -0500

    p5

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
-import update from './update.js';
+import p5 from 'p5';
 
 // even though Rollup is bundling all your files together, errors and
 // logs will still point to your original source modules
 console.log('if you have sourcemaps enabled in your devtools, click on main.js:5 -->');
  
-update(); 
  
-// ok 
+let main = (p) => {
+  p.setup = () =>{
+    // game = new Game(window.innerWidth, window.innerHeight);
+    const cur_speed = 20;
+
+    p.createCanvas(window.innerWidth,window.innerHeight);
+    p.background(40);
+  };
+
+  p.mousePressed = () => {
+    console.log("mouse")
+  };
+};
+
+new p5(main);
