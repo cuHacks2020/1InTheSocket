c3fa1f3b122c7577e6523c2da3b27752cc7cf8a7
Nick <naek2k@outlook.com>
2020-01-11 13:36:25 -0500

    key press events added

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import p5 from 'p5';
 import Game from './Game';
 import io from 'socket.io-client';
 
+let socket = io('http://localhost');
+
 // even though Rollup is bundling all your files together, errors and
 // logs will still point to your original source modules
 console.log('if you have sourcemaps enabled in your devtools, click on main.js:5 -->');
@@ -22,6 +24,23 @@ let main = (p) => {
   p.mousePressed = () => {
     console.log("mouse")
   };
+
+  p.keyPressed = (keyCode) => {
+    if(keyCode.key == "w") { // forward
+      socket.emit("foward")
+    }
+    if(keyCode.key == "a") { // left
+      socket.emit("left")
+    }
+    if(keyCode.key == "d") { // right
+      socket.emit("right")
+    }
+    if(keyCode.key == "s") { // backward
+      socket.emit("backward")
+    }
+  };
 };
+
+
 
 new p5(main);
