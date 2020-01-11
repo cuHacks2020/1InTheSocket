0dc1878a5c3ffd79651d57777f9d6fea903dccf9
Deen <fardeen2000@hotmail.com>
2020-01-11 14:58:33 -0500

    Mid-Rebase

--- src/Game.js
+++ src/Game.js
@@ -21,31 +21,26 @@ export default class Game {
 
     drawMap(p, windowWidth, windowHeight) {
         const pixelsPerBlock = 16;
-        let gridXLength = windowWidth/20;
-        let gridYLength = windowLength/20;
+        let gridXLength = windowWidth / 20;
+        let gridYLength = windowLength / 20;
 
         for (let i = 0; i < this.map.length; i++) {
             for (let j = 0; j < this.map[0].length; j++) {
-                if(this.map[i][j] == "1") {
-                    p.rect(i*gridXLength, j*gridYLength, gridXLength, gridYLength);
+                if (this.map[i][j] == "1") {
+                    p.rect(i * gridXLength, j * gridYLength, gridXLength, gridYLength);
                 }
             }
         }
     }
 
-<<<<<<< HEAD
-    draw(p, windowWidth, windowHeight) {
-        drawMap(p, windowWidth, windowHeight);
-=======
     draw(p) {
         console.log('draw');
-          //have players be ellipses for now
-    
-    this.players.forEach((player) => {
-        player.draw(p);
-    })
+        //have players be ellipses for now
+
+        this.players.forEach((player) => {
+            player.draw(p);
+        })
 
         //drawMap();
->>>>>>> rebased movement stuff
     }
 }
