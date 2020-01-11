9a59c1cddd5c5ff722c8b4a117b45cd416ed42bf
Bill Zhang <billzzhang2@gmail.com>
2020-01-11 14:11:05 -0500

    get map

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,36 @@
-export default class Game {
-  constructor() {
-    console.log('hi');
-  }
+import io from 'socket.io-client';
 
-  draw() {
-    console.log('draw');
-  }
+export default class Game {
+    constructor() {
+        console.log('hi');
+        this.connect();
+
+        this.map = null;
+    }
+    connect() {
+        const socket = io();
+        socket.emit('new user');
+        socket.on("map", this.recvMap);
+    }
+
+    //renders map using a 2d mapObject 
+    recvMap(mapObject) {
+        this.map = mapObject;
+    }
+
+    drawMap(mapObject) {
+        const pixelsPerBlock = 16;
+
+        for (let i = 0; i < this.map.length; i++) {
+            for (let j = 0; j < this.map[0].length; j++) {
+                
+            }
+        }
+    }
+
+    draw(p) {
+        console.log('draw');
+
+        drawMap();
+    }
 }
--- src/main.js
+++ src/main.js
@@ -5,16 +5,19 @@ import io from 'socket.io-client';
 // even though Rollup is bundling all your files together, errors and
 // logs will still point to your original source modules
 console.log('if you have sourcemaps enabled in your devtools, click on main.js:5 -->');
- 
-const game = new Game(); 
+
+const game = new Game();
+
 
 let main = (p) => {
-  p.setup = () =>{
-    p.createCanvas(window.innerWidth,window.innerHeight);
+  p.setup = () => {
+    p.createCanvas(window.innerWidth, window.innerHeight);
     p.background(40);
   };
 
-  p.draw = game.draw;
+  p.draw = () => {
+    game.draw(p);
+  };
 
   p.mousePressed = () => {
     console.log("mouse")
