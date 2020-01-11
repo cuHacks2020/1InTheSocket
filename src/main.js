5b9e5f85a99b72c1352d2350c1990e420373643a
Bill Zhang <billzzhang2@gmail.com>
2020-01-11 11:54:58 -0500

    Bill's changes

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import update from './update.js';
 // even though Rollup is bundling all your files together, errors and
 // logs will still point to your original source modules
 console.log('if you have sourcemaps enabled in your devtools, click on main.js:5 -->');
+console.log('Bill\'s Push');
 
 update();
