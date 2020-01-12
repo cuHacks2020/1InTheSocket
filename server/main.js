311d14cf4bb37d119a3eac8582190674d7186441
Devon Mack <devon.mack@shopify.com>
2020-01-11 23:10:04 -0500

    Bind to heroku port

--- server/main.js
+++ server/main.js
@@ -86,8 +86,8 @@ var map = generateMap();
 
 let players = [];
 
-server.listen(80);
-// WARNING: app.listen(80) will NOT work here!
+const PORT = process.env.PORT || 3000;
+server.listen(PORT);
 
 console.log("GO to http://localhost:80");
 
