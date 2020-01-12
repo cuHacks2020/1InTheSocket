8b53f057d0fe5291e4f3add3210bfcfb741b93d5
Deen <fardeen2000@hotmail.com>
2020-01-11 19:07:47 -0500

    Drawing proper amount of bullets

--- src/Player.js
+++ src/Player.js
@@ -6,10 +6,11 @@ export default class Player {
   constructor() {
     this.x = this.y = 100;
     this.charSpeed = 5;
-    this.bullet = false;
+    // this.bullet = false;
     this.bulletX1 = this.bulletX2 = this.bulletY1 = this.bulletY2 = 0;
     this.bulletAlpha = 1;
-    this.firstRun = true;
+    this.isShooting = false;
+    this.bullets = 5;
   }
 
   shoot(p) {
@@ -17,47 +18,55 @@ export default class Player {
     this.bulletY1 = p.mouseY;
     this.bulletX2 = this.x;
     this.bulletY2 = this.y;
-    this.bullet = true;
-    
+    this.bullets = (this.bullets > 0) ? this.bullets - 1 : this.bullets;
+    this.isShooting = this.bullets > 0;
+    // console.log(this.bullets);
+    // this.bullet = true;
+    // this.bullets = 1;
+
   }
 
   draw(p) {
 
-    if(p.keyIsDown(87)) {
+    if (p.keyIsDown(87)) {
       this.y -= this.charSpeed;
     }
-    if(p.keyIsDown(83)) {
+    if (p.keyIsDown(83)) {
       this.y += this.charSpeed;
     }
-    if(p.keyIsDown(65)) {
+    if (p.keyIsDown(65)) {
       this.x -= this.charSpeed;;
     }
-    if(p.keyIsDown(68)) {
+    if (p.keyIsDown(68)) {
       this.x += this.charSpeed;;
     }
     p.background(40);
-    p.stroke(p.color(0,0,0));
+    p.stroke(p.color(0, 0, 0));
     p.ellipse(this.x, this.y, 50);
     p.fill(p.color(255, 0, 0));
     p.strokeWeight(4);
     //p.line(p.mouseX, p.mouseY, this.x, this.y);
 
     //fade aiming line
-    if(this.bullet == true) {
-      
-      if (this.bulletAlpha > 0)
-      {
-        this.bulletAlpha  -= 0.02;
+    // if (this.bullets > 0 || this.isShooting) {
+    if (this.isShooting) {
+      console.log(this.bullets);
+
+      if (this.bulletAlpha > 0) {
+        this.bulletAlpha -= 0.02;
       }
-      if (this.bulletAlpha < 0)
-      {
+      if (this.bulletAlpha < 0) {
         this.bulletAlpha = 0.001;
-        this.bullet = false;
-        this.bulletAlpha = 1;
+        this.bullets -= 1;
+        this.isShooting = false;
+        // this.bulletAlpha = 1;
       }
-      p.stroke('rgba(0,0,0,' + this.bulletAlpha+ ')');
+      p.stroke('rgba(0,0,0,' + this.bulletAlpha + ')');
       p.line(this.bulletX1, this.bulletY1, this.bulletX2, this.bulletY2);
-      
+
+    }
+    else {
+      this.bulletAlpha = 1;
     }
 
   }
